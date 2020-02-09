059d377edae2c1db323c97864c1672f5e0d17c66
Emil Gadzhiyev <emkamakeoff@gmail.com>
2020-02-10 00:12:54 +0400

    4th commit

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(event) {
+        this.setState({ term: event.target.value })
+    }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <input onChange={this.handleTermChange} placeholder="Start typing" />
+                <button className="SearchButton">Search</button>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
